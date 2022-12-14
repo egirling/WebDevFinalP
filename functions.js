0fae26874acb9782df1cf25b5060195ba0fedd9b
Elizabeth Girling <71190947+egirling@users.noreply.github.com>
2022-12-14 11:34:17 -0700

    started

---
+++ functions.js
@@ -0,0 +1,33 @@
+let api = "P5kvawJa0JTIebx4dPquWg==YQTCY97msGwjjE81";
+
+function getHobby(event){
+    let category = 'general';
+
+    var xhttp2 = new XMLHttpRequest();
+    xhttp2.onreadystatechange = function() {    
+
+       
+        if (this.readyState == 4 && this.status == 200) {
+
+            
+            var hobby = JSON.parse(this.responseText);
+
+            console.log(hobby);
+            document.getElementById("newHobby").innerHTML = hobby.hobby;
+            document.getElementById("learn").innerHTML = hobby.link;
+            
+           
+        } else if (this.readyState == 4) {
+            console.log(this.responseText);
+        }
+    };
+
+    xhttp2.open("GET", 'https://api.api-ninjas.com/v1/hobbies?category=' + category, true);
+
+    xhttp2.setRequestHeader("Content-type", "application/json");
+    xhttp2.setRequestHeader("x-api-key", api);
+    
+    xhttp2.send();
+}
+
+document.getElementById("hobbyButton").addEventListener("click", getHobby);
